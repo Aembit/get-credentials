12165fcea9d7097bcb6af6e7484eb3932ca22fcc
Aditya Pujari <apujari@aembit.io>
2025-11-07 11:57:57 -0600

    (feat): Access Token

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as core from "@actions/core";
 
 import { getIdentityToken } from "./identity-token";
+import { getAccessToken } from "./access-token";
 
 async function run(): Promise<void> {
   try {
@@ -8,7 +9,12 @@ async function run(): Promise<void> {
     const clientId: string = core.getInput("client-id");
     const domain: string = core.getInput("domain");
 
-    const identityToken = await getIdentityToken(clientId, domain);
+    const identityToken: string = await getIdentityToken(clientId, domain);
+    const accessToken: string = await getAccessToken(
+      clientId,
+      identityToken,
+      domain,
+    );
   } catch (error) {
     core.setFailed(`${(error as any)?.message ?? error}`);
   }
