3fe84eff2e64dbe0632f4818ef8dbbe9e5d58fbf
Aditya Pujari <apujari@aembit.io>
2025-11-07 11:32:39 -0600

    (feat): GitHub OIDC identity token

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import * as core from "@actions/core";
 
+import { getIdentityToken } from "./identity-token";
+
 async function run(): Promise<void> {
   try {
+    // Read inputs for action (defined in action.yml file)
+    const clientId: string = core.getInput("client-id");
+    const domain: string = core.getInput("domain");
+
+    const identityToken = await getIdentityToken(clientId, domain);
   } catch (error) {
     core.setFailed(`${(error as any)?.message ?? error}`);
   }
